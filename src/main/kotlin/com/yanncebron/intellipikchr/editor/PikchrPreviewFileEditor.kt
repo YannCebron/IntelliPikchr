56c4ba3e5ca68558033099a889bff053175ad4a4
Yann Cébron <yann.cebron@jetbrains.com>
2021-09-11 21:11:33 +0200

    preview using kroki.io, initial
    known problems:
    - preview doesn't render on initial editor opening sometimes, dummy edit triggers
    - Darcula L&F is unusable

--- src/main/kotlin/com/yanncebron/intellipikchr/editor/PikchrPreviewFileEditor.kt
+++ src/main/kotlin/com/yanncebron/intellipikchr/editor/PikchrPreviewFileEditor.kt
@@ -11,73 +11,120 @@ import com.intellij.openapi.fileEditor.FileEditorState
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.UserDataHolderBase
+import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.ui.components.JBLabel
+import com.intellij.ui.jcef.JCEFHtmlPanel
 import com.intellij.util.Alarm
-import com.intellij.util.ui.UIUtil
 import java.awt.BorderLayout
+import java.awt.event.ComponentAdapter
+import java.awt.event.ComponentEvent
 import java.beans.PropertyChangeListener
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.util.*
+import java.util.zip.Deflater
 import javax.swing.JComponent
 import javax.swing.JPanel
 
-class PikchrPreviewFileEditor(private val project: Project, file: VirtualFile) : UserDataHolderBase(),
+
+class PikchrPreviewFileEditor(project: Project, private val virtualFile: VirtualFile) : UserDataHolderBase(),
     FileEditor {
 
-    private val virtualFile: VirtualFile
-    private val document: Document?
+    private val document: Document? = FileDocumentManager.getInstance().getDocument(virtualFile)
 
-    private val panel: JPanel
-    private val previewLabel: JBLabel
+    private val panel: JPanel;
+    private var jcefPanel: JCEFHtmlPanel? = null
 
-    private val pooledAlarm = Alarm(Alarm.ThreadToUse.POOLED_THREAD, this)
-    private val swingAlarm = Alarm(Alarm.ThreadToUse.SWING_THREAD, this)
+    private val docAlarm = Alarm(Alarm.ThreadToUse.POOLED_THREAD, this)
+    private val previewAlarm = Alarm(Alarm.ThreadToUse.SWING_THREAD, this)
 
     init {
-        virtualFile = file
-        document = FileDocumentManager.getInstance().getDocument(virtualFile)
-
         document?.addDocumentListener(object : DocumentListener {
             override fun beforeDocumentChange(event: DocumentEvent) {
-                pooledAlarm.cancelAllRequests()
+                docAlarm.cancelAllRequests()
             }
 
             override fun documentChanged(event: DocumentEvent) {
-                pooledAlarm.addRequest({ updatePreview() }, PREVIEW_UPDATE_DELAY)
+                docAlarm.addRequest({ updatePreview() }, TYPING_UPDATE_DELAY)
             }
         }, this)
 
         panel = JPanel(BorderLayout())
-        previewLabel = JBLabel("Loading preview...", UIUtil.ComponentStyle.REGULAR, UIUtil.FontColor.BRIGHTER)
+        panel.addComponentListener(object : ComponentAdapter() {
+            override fun componentShown(e: ComponentEvent?) {
+                previewAlarm.addRequest({
+                    if (jcefPanel == null) {
+                        attachPreview()
+                    }
+                }, 0, ModalityState.stateForComponent(component))
+            }
 
+            override fun componentHidden(e: ComponentEvent?) {
+                previewAlarm.addRequest({
+                    if (jcefPanel != null) {
+                        detachPreview()
+                    }
+                }, 0, ModalityState.stateForComponent(component))
+            }
+        })
 
-        panel.add(previewLabel, BorderLayout.CENTER)
+        attachPreview()
+    }
 
+    private fun attachPreview() {
+        jcefPanel = JCEFHtmlPanel("")
+        jcefPanel!!.cefBrowser.createImmediately() // todo makes it _a bit_ more reliable to show up on editor load
+        jcefPanel!!.setHtml("<em>Initializing preview...</em>")
+        panel.add(jcefPanel!!.component, BorderLayout.CENTER)
         initPreview()
     }
 
+    private fun detachPreview() {
+        if (jcefPanel != null) {
+            panel.remove(jcefPanel!!.component)
+            Disposer.dispose(jcefPanel!!)
+            jcefPanel = null
+        }
+    }
+
     private fun updatePreview() {
-        if (document == null || !virtualFile.isValid || Disposer.isDisposed(this)) {
+        if (jcefPanel == null || document == null || !virtualFile.isValid || Disposer.isDisposed(this)) {
             return
         }
 
-        swingAlarm.cancelAllRequests()
-        swingAlarm.addRequest(
-            { previewLabel.text = document.text },
-            PREVIEW_UPDATE_DELAY * 2,
-            ModalityState.stateForComponent(component)
+        previewAlarm.cancelAllRequests()
+        val runnable = Runnable {
+            if (jcefPanel == null) return@Runnable
+
+            val encodedDiagram = encode(
+                document.text
+                        + StringUtil.repeat(" ", 100) // todo fails for "too short input"
+            )
+//                println(encodedDiagram)
+
+            jcefPanel!!.cefBrowser.loadURL("https://kroki.io/pikchr/svg/$encodedDiagram")
+        }
+        previewAlarm.addRequest(
+            runnable,
+            PREVIEW_UPDATE_DELAY, ModalityState.stateForComponent(component)
         )
     }
 
     private fun initPreview() {
-        pooledAlarm.cancelAllRequests()
-        pooledAlarm.addRequest({ updatePreview() }, 0)
+        docAlarm.cancelAllRequests()
+        docAlarm.addRequest({ updatePreview() }, 0)
     }
 
     override fun selectNotify() {
-        initPreview()
+        if (jcefPanel != null) {
+            initPreview()
+        }
     }
 
     override fun dispose() {
+        if (jcefPanel != null) {
+            Disposer.dispose(jcefPanel!!)
+        }
     }
 
     override fun getComponent(): JComponent {
@@ -85,7 +132,7 @@ class PikchrPreviewFileEditor(private val project: Project, file: VirtualFile) :
     }
 
     override fun getPreferredFocusedComponent(): JComponent? {
-        return panel
+        return if (jcefPanel != null) jcefPanel!!.component else null
     }
 
     override fun getName(): String {
@@ -105,6 +152,27 @@ class PikchrPreviewFileEditor(private val project: Project, file: VirtualFile) :
     override fun getCurrentLocation(): FileEditorLocation? = null
 
     companion object {
-        const val PREVIEW_UPDATE_DELAY = 50
+        const val TYPING_UPDATE_DELAY = 100
+        const val PREVIEW_UPDATE_DELAY = 20
+
+        @Throws(IOException::class)
+        private fun encode(decoded: String): String {
+            return String(Base64.getUrlEncoder().encode(compress(decoded.toByteArray(Charsets.UTF_8))), Charsets.UTF_8)
+        }
+
+        @Throws(IOException::class)
+        private fun compress(source: ByteArray): ByteArray? {
+            val result = ByteArray(2048)
+            val deflater = Deflater(Deflater.BEST_COMPRESSION)
+            deflater.setInput(source, 0, source.size)
+            deflater.finish()
+            val compressedLength = deflater.deflate(result, 0, source.size, Deflater.FULL_FLUSH)
+            deflater.end()
+            ByteArrayOutputStream().use { byteArrayOutputStream ->
+                byteArrayOutputStream.write(result, 0, compressedLength)
+                return byteArrayOutputStream.toByteArray()
+            }
+        }
+
     }
 }
