b132b8ed7ecfc5af5946aa2007425b6f74321bfc
Yann Cébron <yann.cebron@jetbrains.com>
2024-03-31 14:58:38 +0200

    PikchrTokenTypesTest: cleanup

--- src/test/kotlin/com/yanncebron/intellipikchr/lang/psi/PikchrTokenTypesTest.kt
+++ src/test/kotlin/com/yanncebron/intellipikchr/lang/psi/PikchrTokenTypesTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 The Authors
+ * Copyright 2024 The Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,54 +18,52 @@ package com.yanncebron.intellipikchr.lang.psi
 
 import com.intellij.psi.tree.IElementType
 import com.intellij.psi.tree.TokenSet
-import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import com.intellij.testFramework.UsefulTestCase
 import com.yanncebron.intellipikchr.lang.PikchrLanguage
 import junit.framework.TestCase
 
-class PikchrTokenTypesTest : BasePlatformTestCase() {
+class PikchrTokenTypesTest : UsefulTestCase() {
 
-    companion object {
-        private val GROUPS = arrayOf(
-                PikchrTokenGroups.FUNCTION,
-                PikchrTokenGroups.KEYWORDS,
-                PikchrTokenGroups.DIRECTION,
-                PikchrTokenGroups.OPERATION_SIGN,
-                PikchrTokenGroups.OBJECT,
-                PikchrTokenGroups.ATTRIBUTE,
-                PikchrTokenGroups.EDGE_POSITION,
-                PikchrTokenGroups.ARROW_HEAD,
-                PikchrTokenGroups.DEBUG_STATEMENTS
-        )
+    private val groups = arrayOf(
+        PikchrTokenGroups.FUNCTION,
+        PikchrTokenGroups.KEYWORDS,
+        PikchrTokenGroups.DIRECTION,
+        PikchrTokenGroups.OPERATION_SIGN,
+        PikchrTokenGroups.OBJECT,
+        PikchrTokenGroups.ATTRIBUTE,
+        PikchrTokenGroups.EDGE_POSITION,
+        PikchrTokenGroups.ARROW_HEAD,
+        PikchrTokenGroups.DEBUG_STATEMENTS
+    )
 
-        private val IGNORE_TYPES = TokenSet.create(
-                PikchrTokenTypes.LINE_COMMENT,
-                PikchrTokenTypes.BLOCK_COMMENT,
-                PikchrTokenTypes.LINEFEED,
-                PikchrTokenTypes.BACKSLASH,
-                PikchrTokenTypes.LABEL,
-                PikchrTokenTypes.VARIABLE,
-                PikchrTokenTypes.STRING,
-                PikchrTokenTypes.DECIMAL,
-                PikchrTokenTypes.FLOAT,
-                PikchrTokenTypes.HEX,
-                PikchrTokenTypes.ROUND_L, PikchrTokenTypes.ROUND_R,
-                PikchrTokenTypes.BRACES_L, PikchrTokenTypes.BRACES_R,
-                PikchrTokenTypes.BRACKET_L, PikchrTokenTypes.BRACKET_R,
-                PikchrTokenTypes.COMMA,
-                PikchrTokenTypes.DOT,
-                PikchrTokenTypes.SEMICOLON,
-                PikchrTokenTypes.COLON,
-                PikchrTokenTypes.PERCENT,
-                PikchrTokenTypes.DOLLAR
-        )
-    }
+    private val ignoreTypes = TokenSet.create(
+        PikchrTokenTypes.LINE_COMMENT,
+        PikchrTokenTypes.BLOCK_COMMENT,
+        PikchrTokenTypes.LINEFEED,
+        PikchrTokenTypes.BACKSLASH,
+        PikchrTokenTypes.LABEL,
+        PikchrTokenTypes.VARIABLE,
+        PikchrTokenTypes.STRING,
+        PikchrTokenTypes.DECIMAL,
+        PikchrTokenTypes.FLOAT,
+        PikchrTokenTypes.HEX,
+        PikchrTokenTypes.ROUND_L, PikchrTokenTypes.ROUND_R,
+        PikchrTokenTypes.BRACES_L, PikchrTokenTypes.BRACES_R,
+        PikchrTokenTypes.BRACKET_L, PikchrTokenTypes.BRACKET_R,
+        PikchrTokenTypes.COMMA,
+        PikchrTokenTypes.DOT,
+        PikchrTokenTypes.SEMICOLON,
+        PikchrTokenTypes.COLON,
+        PikchrTokenTypes.PERCENT,
+        PikchrTokenTypes.DOLLAR
+    )
 
     fun testAllTokenTypesRegisteredInOneGroup() {
         for (type in IElementType.enumerate { t -> t.language == PikchrLanguage.INSTANCE }) {
-            if (IGNORE_TYPES.contains(type)) continue
+            if (ignoreTypes.contains(type)) continue
 
             var groupMatch = 0
-            for (group in GROUPS) {
+            for (group in groups) {
                 if (group.contains(type)) groupMatch++
             }
 
