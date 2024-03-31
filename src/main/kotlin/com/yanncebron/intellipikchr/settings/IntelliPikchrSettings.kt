0b591dc1cb766f7022d446e5a5617c916930c6a9
Yann Cébron <yann.cebron@jetbrains.com>
2024-03-31 15:56:59 +0200

    IntelliPikchrSettings: convert to light service

--- src/main/kotlin/com/yanncebron/intellipikchr/settings/IntelliPikchrSettings.kt
+++ src/main/kotlin/com/yanncebron/intellipikchr/settings/IntelliPikchrSettings.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 The Authors
+ * Copyright 2024 The Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@ import com.intellij.openapi.components.*
 import com.intellij.openapi.project.Project
 import com.intellij.util.messages.Topic
 
+@Service(Service.Level.PROJECT)
 @State(name = "IntelliPikchrSettings", storages = [Storage(StoragePathMacros.WORKSPACE_FILE)])
 internal class IntelliPikchrSettings(private val project: Project) :
     SimplePersistentStateComponent<IntelliPikchrSettingsState>(
